f2fe40dca5038aff4c3bcc2397983578d4b12ef6
corn-cloud <comowaiye@gmail.com>
2020-02-20 18:07:49 -0600

    crazy

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,46 @@
 class FiguresController < ApplicationController
   # add controller methods
+  set :views, 'app/views/figures'
+  
+  get '/figures' do
+    @figures = Figure.all 
+    erb :index
+  end
+  
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :new
+  end
+  
+  get '/figures/:id' do
+      @figure = current_figure
+      erb :show
+  end
+
+      get '/figures/:id/edit' do
+        @figure = current_figure
+        @titles = Title.all
+        @landmarks = Landmark.all
+        erb :edit
+      end
+
+  post "/figures" do
+    # binding.pry
+   figure =  Figure.create(params[:figure])
+  #  figure.titles << Title.create(params[:title])
+    
+    redirect '/figures'
+  end
+
+  patch '/figures/:id' do
+    figure = current_figure
+    figure.update( params[:figure])
+    redirect "/figures/#{figure.id}"
+  end
+
+    def current_figure
+      Figure.find(params[:id])
+    end
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,11 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  set :views, "app/views/landmarks"
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :index
+  end
+
+  
 end
